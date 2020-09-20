01883e974031fb35935c558504236945fb548199
sfah007 <69042482+sfah007@users.noreply.github.com>
2020-09-21 02:25:17 +0300

    Update web.php

--- web.php
+++ web.php
@@ -1,16 +1,29 @@
-# ch :- @php_i # Dev :- @dev_a #
-
-# تنشر اذكر المصدر #
-
 <?php
-if($_GET['user']){
- $_GET['user'] = trim($_GET['user'],'@');
-  $dev_a = file_get_contents('http://t.me/'.$_GET['user'],null,null,0,1334);
-preg_match('/property="og:description" content="(.*)">/',$dev_a,$match);
-if($match[1] == ""){
-echo "متاح ✔️";
-}else{
-echo "غير متاح ✖️";
+<!-- telegram: @corup / @api_web-->
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-}  
-# ch :- @php_i # Dev :- @dev_a #
+$admin = '@$ADMIN';
+include 'madeline.php';
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+$user = readline("UserName: @");
+$start = time();
+$x = 0;
+$MadelineProto->messages->sendMessage(['peer' => $admin, 'message' => "▪Turbo (CORUP) Has Been Launched"]);
+do{
+try{
+ob_start();
+$MadelineProto->messages->getPeerDialogs(['peers'=> [$user]]);
+echo 'No:'.$user." - ".$x." Bad in:".date('s')."\n";
+$x++;
+}catch(Exception $e){
+$MadelineProto->account->updateUsername(['username' => $user]);
+$end = time() - $start;
+$MadelineProto->messages->sendMessage(['peer' => $admin, 'message' => "▪User-Taken -> @$user"]);
+echo "DONE -> @$user \n";
+exit;
+}
+ob_end_flush();
+}while(true);
+<!-- telegram: @corup / @api_web-->
