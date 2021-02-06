51f4dc625d64944f1fc984dd8988b07686b5d88d
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-02-06 13:03:29 +0300

    Update short.php

--- short.php
+++ short.php
@@ -143,7 +143,7 @@ if($text == "/start"){
  ]);
  }
 
- @$link = file_get_contents("https://0i.is/api/?key=egSFcZ5liPZr&url=$text&format=sample");
+ @$link = file_get_contents("https://0i.is/api/?key=egSFcZ5liPZr&url=$text");
  if($text !="/start"){
  bot('sendMessage',[
  'chat_id'=>$chat_id,
