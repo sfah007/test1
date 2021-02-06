1a2c4042c1ff9561e2c52a834f43123ba1fe2938
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-02-06 13:22:40 +0300

    Update short.php

--- short.php
+++ short.php
@@ -1,59 +1,169 @@
-<?php
-define('1092156256:AAG5VSCrui6MjxNN6_WSIr3YqZqpDcwqzFk','توكن');
-$admin = 201839212;
-function roonx($method,$datas=[]){
-    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
-    $res = curl_exec($ch);
-    if(curl_error($ch)){
-        var_dump(curl_error($ch));
-    }else{
-        return json_decode($res);
-    }
-}
-$update = json_decode(file_get_contents('php://input'));
-$chat_id = $update->message->chat->id;
-$text = $update->message->text;
-$from = $update->message->from->id;
-$step = file_get_contents('step.txt');
-  if(preg_match('/^([Hh]ttp|[Hh]ttps)(.*)/',$text)){
-    $short = file_get_contents('https://0i.is/api/?key=egSFcZ5liPZr&url='.$text);
-    roonx('sendMessage',[
-      'chat_id'=>$chat_id,
-      'text'=>"رابط الخااص بك 🤓:\n".$short."\n ",
-      'parse_mode'=>'HTML'
-    ]);
-  }
-  if(preg_match('/^\/([sS]tart)/',$text) and $from == $admin){
-	  roonx('sendMessage',[
-      'chat_id'=>$chat_id,
-      'text'=>"مرحبا صديقي انا بوت اختصر لك روابط
-ارسل رابط فقط ‼️
-مثال : http//:Google.com
+<?php  
 
-لمعرفة مشتركين بوت ارسل /stats
-تابع @b7_78)",
-      'parse_mode'=>'HTML'
-    ]);
+//» كـاتب الملف/»سجاد العراقي «//
+//ch √√ @LUA2PHP
+//مـن تخمـط الملـف اذكر المصدر//
+//ولا تغـير بلحقوق وتظهر فشلك//
+//@SJAD100 /المطـور/ 
+// اشترك بقناه المطور من فضلك//
+ob_start(); 
+$API_KEY = "1092156256:AAG5VSCrui6MjxNN6_WSIr3YqZqpDcwqzFk";#توكن البوت 
+define('API_KEY',$API_KEY); 
+echo "<a href='https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']."'>setwebhook</a>"; 
+echo file_get_contents("https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']); 
+function bot($method,$datas=[]){ 
+$url = "https://api.telegram.org/bot".API_KEY."/".$method; 
+$ch = curl_init(); 
+curl_setopt($ch,CURLOPT_URL,$url); curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
+curl_setopt($ch,CURLOPT_POSTFIELDS,$datas); 
+$res = curl_exec($ch); 
+if(curl_error($ch)){ 
+var_dump(curl_error($ch)); 
+}else{return json_decode($res);}} 
+$update   = json_decode(file_get_contents('php://input'));
+$message    = $update->message;
+$text       = $message->text;
+$chat_id    = $message->chat->id;
+$from_id         = $message->from->id;
+$user       = '@'.$message->from->username;
+$nrame     = $message->from->first_name;
+$username = $message->from->username;
+$data       = $update->callback_query->data;
+$chat_id2   = $update->callback_query->message->chat->id;
+$message_id = $update->callback_query->message->message_id;
+
+
+
+///////////////////
+
+$u = explode("\n",file_get_contents("pj.txt"));
+$c = count($u)-1;
+$modxe = file_get_contents("em.txt");
+$admin = "201839212"; /* ايدي مالتك */
+#                بداية الاوامر                 #
+if ($update && !in_array($chat_id, $u)) {
+    file_put_contents("pj.txt", $chat_id."\n",FILE_APPEND);
   }
-if(preg_match('/^\/([Ss]tats)/',$text) and $from == $admin){
-    $user = file_get_contents('users.txt');
-    $member_id = explode("\n",$user);
-    $member_count = count($member_id) -1;
-    roonx('sendMessage',[
-      'chat_id'=>$chat_id,
-      'text'=>"<b>اعضاء البوت</b> : <b>$member_count</b>",
-      'parse_mode'=>'HTML'
+if ($text == "/start" and $chat_id == $admin ) {
+    bot('sendMessage',[
+        'chat_id'=>$chat_id,
+      'text'=>"
+☑️￤اهلا عزيزي :- المطور .
+▫️￤اليك الاوامر الان حدد ماتريده 📩
+-
+",
+'parse_mode'=>"MarkDown",
+'disable_web_page_preview'=>true,
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+[['text'=>'رسالة للكل ','callback_data'=>'ce']],
+[['text'=>'عدد الاعضاء ','callback_data'=>'co']],
+            ]
+            ])
+        ]);
+}
+// ماثيو( سجاد العراقي )𖤍
+if($data == 'off'){
+bot('editMessageText',[
+'chat_id'=>$chat_id2,
+'message_id'=>$message_id,
+      'text'=>"
+☑️￤اهلا عزيزي :- المطور .
+▫️￤اليك الاوامر الان حدد ماتريده 📩
+-
+",
+'parse_mode'=>"MarkDown",
+'disable_web_page_preview'=>true,
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+[['text'=>'رسالة للكل ','callback_data'=>'ce']],
+[['text'=>'عدد الاعضاء ','callback_data'=>'co']],
+            ]
+            ])
+]);
+file_put_contents('em.txt', '');
+}
+#                   المشتركين                   #
+if($data == "co" and $update->callback_query->message->chat->id == $admin ){ 
+    bot('answercallbackquery',[
+        'callback_query_id'=>$update->callback_query->id,
+        'text'=>"
+        عدد مشتركين البوت📢 :- [ $c ] .
+        ",
+        'show_alert'=>true,
+]);
+}
+#                   رسالة للكل                   #
+if($data == "ce" and $update->callback_query->message->chat->id == $admin){ 
+    file_put_contents("em.txt","yas");
+    bot('EditMessageText',[
+    'chat_id'=>$update->callback_query->message->chat->id,
+    'message_id'=>$update->callback_query->message->message_id,
+    'text'=>"▪️ ارسل رسالتك الان 📩 وسيتم نشرها لـ [ $c ] مشترك . 
+   ",
+    'reply_markup'=>json_encode([
+        'inline_keyboard'=>[
+[['text'=>' الغاء 🚫 •','callback_data'=>'off']]    
+        ]
+    ])
     ]);
 }
-$user = file_get_contents('users.txt');
-    $members = explode("\n",$user);
-    if (!in_array($chat_id,$members)){
-      $add_user = file_get_contents('users.txt');
-      $add_user .= $chat_id."\n";
-     file_put_contents('users.txt',$add_user);
-    }
-	?>
+#------ #كتابة #سجاد ------#
+if($text and $modxe == "yas" and $chat_id == $admin ){
+    for ($i=0; $i < count($u); $i++) { 
+        bot('sendMessage',[
+          'chat_id'=>$u[$i],
+          'text'=>"$text",
+'parse_mode'=>"MarkDown",
+'disable_web_page_preview'=>true,
+
+]);
+    file_put_contents("em.txt","no");
+
+} 
+}
+
+if($text == "/start"){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ "text"=>
+"♳» اهــلا بـك فـي بـوت اختصار الروابط ㋡
+♴» البوت يقوم بختصار الروابط ♡
+♵»البـوت سـريــع جـدا وجـديــد ᗧ
+♵»فقط قم بأرسال الرابط وانتظر ≿
+♶»سوف يرسل لك رابط مختصر 💖
+♻️» #ملاحظه:- يجب ان يحتوي الرابط ع * https *
+♷»تابعنا  » @LUA2PHP",
+'reply_markup'=>json_encode([
+        'inline_keyboard'=>[
+[['text'=>' 📡تابعنا •','url'=>'t.me/lua2php']]    ,
+[['text'=>' 📚 المطور •','url'=>'t.me/sjad100']] ,   
+        ]
+    ])
+ ]);
+ }
+
+ @$link = file_get_contents("http://llink.ir/yourls-api.php?signature=a13360d6d8&action=shorturl&url=$text&format=sample");
+ if($text !="/start"){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ "text"=>"
+♳» هذا الرابط الخاص بك ㋡
+♴» اضغط ع الرابط وسوف ينسخ ♡
+♵»الرابط 
+`$link`   ᗧ",
+'parse_mode'=>"MarkDown",
+ 'reply_markup'=>json_encode([
+        'inline_keyboard'=>[
+[['text'=>' 📡تابعنا •','url'=>'t.me/lua2php']]    ,
+        ]
+    ])
+ ]);
+ }
+ 
+ //» كـاتب الملف/»سجاد العراقي «//
+//ch √√ @LUA2PHP
+//مـن تخمـط الملـف اذكر المصدر//
+//ولا تغـير بلحقوق وتظهر فشلك//
+//@SJAD100 /المطـور/ 
+ 
